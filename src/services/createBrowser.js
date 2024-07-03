3cac713767e9a57c3ba25ed5fb3904f1d2dbfa05
mushroom <hectorvaldesm47@gmail.com>
2024-07-02 20:18:37 -0400

    Adding the executable path in createBrowser

--- src/services/createBrowser.js
+++ src/services/createBrowser.js
@@ -5,6 +5,7 @@ async function createBrowser(){
     try{
         const browser = await puppeteer.launch({
             headless: true,
+            executablePath: '/usr/bin/google-chrome',
             args: [
               '--no-sandbox',
               '--disable-setuid-sandbox',
