e06511b46c3c843adc6c2f34f46b4dfbd6c707a1
mushroom <hectorvaldesm47@gmail.com>
2024-07-03 19:48:26 -0400

    Adding some parameters

--- src/services/createBrowser.js
+++ src/services/createBrowser.js
@@ -5,21 +5,11 @@ async function createBrowser(){
     try{
         const browser = await puppeteer.launch({
             headless: true,
-            // executablePath: '/usr/bin/chromium-browser',
+            executablePath: '/usr/bin/chromium-browser',
             args: [
               '--no-sandbox',
               '--disable-setuid-sandbox',
-              '--disable-dev-shm-usage', // Reduce el uso de /dev/shm
-              '--single-process', // Ejecuta todo en un solo proceso
-              '--disable-accelerated-2d-canvas',
               '--disable-gpu',
-              '--disable-notifications', // Deshabilita las notificaciones
-              '--disable-crash-reporter', // Deshabilita el reporte de crashes
-              '--disable-sync', // Deshabilita la sincronización
-              '--disable-background-networking', // Deshabilita las conexiones de red en segundo plano
-              '--disable-background-timer-throttling', // Deshabilita la reducción de temporizadores en segundo plano
-              '--disable-backgrounding-occluded-windows', // Deshabilita el procesamiento en segundo plano de ventanas ocultas
-              '--disable-renderer-backgrounding' // Deshabilita el procesamiento en segundo plano del renderizador
             ]
           });
 
