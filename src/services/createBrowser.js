ec1f39c95123d1c05ad19972ed4f71a541d36945
mushroom <hectorvaldesm47@gmail.com>
2024-07-04 21:24:44 -0400

    Adding some args

--- src/services/createBrowser.js
+++ src/services/createBrowser.js
@@ -9,7 +9,17 @@ async function createBrowser(){
             args: [
               '--no-sandbox',
               '--disable-setuid-sandbox',
+              '--disable-dev-shm-usage', // Reduce el uso de /dev/shm
+              '--single-process', // Ejecuta todo en un solo proceso
+              '--disable-accelerated-2d-canvas',
               '--disable-gpu',
+              '--disable-notifications', // Deshabilita las notificaciones
+              '--disable-crash-reporter', // Deshabilita el reporte de crashes
+              '--disable-sync', // Deshabilita la sincronización
+              '--disable-background-networking', // Deshabilita las conexiones de red en segundo plano
+              '--disable-background-timer-throttling', // Deshabilita la reducción de temporizadores en segundo plano
+              '--disable-backgrounding-occluded-windows', // Deshabilita el procesamiento en segundo plano de ventanas ocultas
+              '--disable-renderer-backgrounding' // Deshabilita el procesamiento en segundo plano del renderizador
             ]
           });
 
