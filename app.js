b5659a0599686d64dd18d341437666a26f79fa42
Mushroom <hectorvaldesm47@gmail.com>
2024-07-08 09:36:24 -0400

    Trying again with another test

--- app.js
+++ app.js
@@ -2,73 +2,48 @@ const { updateProductList } = require('./src/services/updateProductList');
 const { getProductPrice } = require('./src/services/getProductPrice');
 const { createLogMessage } = require('./src/utils/createLog');
 const { listadoUrls } = require('./src/assets/ListadoUrlProductos');
-// const { createBrowser } = require('./src/services/createBrowser');
-
 const puppeteer = require('puppeteer');
-// const { createLogMessage } = require('../utils/createLog');
-
-// async function createBrowser(){
-//     try{
-//         const browser = await puppeteer.launch({
-//             headless: true,
-//             // executablePath: '/usr/bin/chromium-browser',
-//             args: [
-//               '--no-sandbox',
-//               '--disable-setuid-sandbox',
-//             ]
-//           });
-
-//           return browser;
-
-//     }catch(err){
-//         createLogMessage(`Error al crear el navegador: ${err}`);
-//         return null;
-//     }
-// }
 
 async function main() {
-  // let browser;
-  
+  let browser;
+
   try {
     createLogMessage('Inicio de script');
-    // browser = await createBrowser();
-    const browser = await puppeteer.launch({
-                  headless: true,
-                  // executablePath: '/usr/bin/chromium-browser',
-                  args: [
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                  ]
-                });
+    browser = await puppeteer.launch({
+      headless: true,
+      // executablePath: '/usr/bin/chromium-browser',
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ]
+    });
 
     if (!browser) {
       console.log('Error: El objeto browser es null');
       return; // Termina la ejecución de la función main
     }
     
-      // Map para crear un array de promesas
-      const promises = listadoUrls.map(async (url) => {
-        
-        try {
-          const productData = await getProductPrice(url, browser);
-          if (productData) {
-            await updateProductList(productData);
-          } else {
-            createLogMessage(`No se encontraron datos para URL: ${url}`);
-          }
-        } catch (error) {
-          createLogMessage(`Error procesando URL: ${url} - ${error.message}`);
+    // Map para crear un array de promesas
+    const promises = listadoUrls.map(async (url) => {
+      try {
+        const productData = await getProductPrice(url, browser);
+        if (productData) {
+          await updateProductList(productData);
+        } else {
+          createLogMessage(`No se encontraron datos para URL: ${url}`);
         }
-      });
-  
-      // Espera a que todas las promesas se resuelvan
-      await Promise.all(promises);
-      await browser.close();
-      createLogMessage("El Script terminó de ejecutarse \n-------------------------------------------");
+      } catch (error) {
+        createLogMessage(`Error procesando URL: ${url} - ${error.message}`);
+      }
+    });
+
+    // Espera a que todas las promesas se resuelvan
+    await Promise.all(promises);
+    createLogMessage("El Script terminó de ejecutarse \n-------------------------------------------");
 
   } catch (error) {
     createLogMessage(`Error en main: ${error.message}`);
-  }finally {
+  } finally {
     if (browser) {
       await browser.close();
     }
--- src/services/createBrowser.js
+++ src/services/createBrowser.js
@@ -5,7 +5,6 @@ async function createBrowser(){
     try{
         const browser = await puppeteer.launch({
             headless: true,
-            // executablePath: '/usr/bin/chromium-browser',
             args: [
               '--no-sandbox',
               '--disable-setuid-sandbox',
